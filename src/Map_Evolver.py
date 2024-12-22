9967e4b50a3390ebf31b219219d910db423535be
r1407p <buszter0403@gmail.com>
2024-12-22 08:52:57 +0000

    feat: add connection into the fitness function

--- src/Map_Evolver.py
+++ src/Map_Evolver.py
@@ -2,11 +2,11 @@ from src.RPGMAP import RPGMAP
 from src.utils import MAP_WIDTH, MAP_HEIGHT, IMAGES
 from tqdm import tqdm
 import random
-
+from copy import deepcopy
 class MapEvolver(object):
     def __init__(self, width, height, 
                  population_size, generations, 
-                 mutation_rate, selection_size
+                 mutation_rate, selection_size,
                  elite_rate=0.4):
         self.width = width
         self.height = height
@@ -15,12 +15,42 @@ class MapEvolver(object):
         self.init_mutation_rate = mutation_rate
         self.selection_size = selection_size
         self.tiles = list(IMAGES.keys())
+        self.elite_rate = elite_rate
 
         self.population = [RPGMAP(width, height) for _ in range(population_size)]
         self.fitness = [None for _ in range(population_size)]
 
-    def evaluate_fitness(self):
-        pass
+    def evaluate_fitness(self, map: RPGMAP):
+        map_data = map.getmap()
+        def connections(map_data):
+            temp_map = deepcopy(map_data)
+            for x in range(self.width):
+                for y in range(self.height):
+                    if temp_map[y][x] == '4': # riverstone is considered as river in this case(connection)
+                        temp_map[y][x] = '1'
+            def dfs(x, y , tile):
+                if x < 0 or x >= self.width or y < 0 or y >= self.height:
+                    return 1
+                if temp_map[y][x] != tile:
+                    return 0
+                temp_map[y][x] = 'x'
+                reward = 1 + dfs(x + 1, y, tile) + dfs(x - 1, y, tile) + dfs(x, y + 1, tile) + dfs(x, y - 1, tile)
+                if tile == '1':
+                    reward /= 2
+                return reward
+
+            connections = []
+            for x in range(self.width):
+                for y in range(self.height):
+                    if temp_map[y][x] != 'x':
+                        connections.append(dfs(x, y, temp_map[y][x]))
+            return connections
+        connections_num = connections(map_data)
+        connections_scores = [x * x for x in connections_num]
+
+        score = sum(connections_scores)
+        return score
+
 
     def mutate(self, map: RPGMAP, mtuation_rate: float):
         """
@@ -45,7 +75,37 @@ class MapEvolver(object):
         return RPGMAP(self.width, self.height, child_map)
 
     def evolve(self):
-        pass
+        for i in range(self.population_size):
+            self.fitness[i] = self.evaluate_fitness(self.population[i])
+
+        for generation in tqdm(range(self.generations)):
+            mutation_rate = self.init_mutation_rate * (1 - generation / self.generations) # linearly decrease the mutation rate
+
+            new_population = []
+            for _ in range(self.population_size):
+                parent1 = self.selection()
+                parent2 = self.selection()
+                child = self.crossover(parent1, parent2)
+                child = self.mutate(child, mutation_rate)
+                new_population.append(child)
+
+            old_population = self.population
+            old_fitness = self.fitness 
+
+            new_population = new_population
+            new_fitness = [None for _ in range(self.population_size)]
+            
+            for i in range(self.population_size):
+                new_fitness[i] = self.evaluate_fitness(new_population[i])
+
+            old_population = sorted(zip(old_population, old_fitness), key=lambda x: x[1], reverse=True)
+            new_population = sorted(zip(new_population, new_fitness), key=lambda x: x[1], reverse=True)
+
+            population = old_population[:int(self.population_size * self.elite_rate)] + new_population[:int(self.population_size * (1 - self.elite_rate))]
+            self.population = [x[0] for x in population]
+            self.fitness = [x[1] for x in population]
+
+        return self.get_best_map()
 
     def selection(self):
         """
